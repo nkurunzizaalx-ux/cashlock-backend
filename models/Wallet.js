5e7211f844ab2ad569fda81696837dc95473d3ad
Shema <shemsonex@gmail.com>
2025-11-30 18:08:17 +0200

    Added wallet routes

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const WalletSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },   // FIXED
   balance: { type: Number, default: 0 },
   currency: { type: String, default: "RWF" },
 
